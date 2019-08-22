50e65b7bcad7c628c63516fd550431b562e5c272
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-22 11:02:05 +0100

    first commit

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,16 @@
+def roman_numerals(number)
+  numerals = {
+    1 => "I",
+    2 => "II",
+    3 => "III",
+    4 => "IV",
+    5 => "V",
+    6 => "VI",
+    7 => "VII",
+    8 => "VIII",
+    9 => "IX",
+    10 => "X"
+  }
+
+  numerals[number]
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,41 @@
+require 'roman_numerals'
+
+describe 'roman_numerals' do
+it {expect(roman_numerals(1)).to eq("I")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(2)).to eq("II")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(3)).to eq("III")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(4)).to eq("IV")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(5)).to eq("V")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(6)).to eq("VI")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(7)).to eq("VII")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(8)).to eq("VIII")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(9)).to eq("IX")}
+end
+
+describe 'roman_numerals' do
+  it {expect(roman_numerals(10)).to eq("X")}
+end
